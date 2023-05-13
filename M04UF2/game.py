584912cee199282e98492bd3381018e3f4044590
ADRIAVIDOSA <adriavidosa2001@gmail.com>
2023-05-13 19:25:28 +0200

    limpieza 2

--- M04UF2/game.py
+++ M04UF2/game.py
@@ -1,75 +1,94 @@
 #!/usr/bin/python3
 import xmltodict
+import json
 import os
 import random	
 
-enemy_files = "enemy_xml"
+ENEMY_FILES_XML = "enemy_xml.xml"
+ENEMY_FILES_JSON = "enemy_json.json"
+SAVE_XML = "game_save.xml"
+SAVE_JSON = "game_save.json"
 
-print("BIENVENIDO A NUESTRO JUEGO")
+class Game:
+    def __init__(self):
+        self.enemy_dict = None
+        self.player = None
+        self.current_enemy = None
+        self.eliminated_enemies = []
+        self.load_enemy_file()
+        self.load_game()
 
-nombre = input("Como te llamas? ")
-difficulty = ""
-player_health = 0
-player_damage = 0
+    def load_enemy_file(self):
+        with open(ENEMY_FILES_XML, "r") as f:
+            xml_string = f.read()
+        self.enemy_dict = xmltodict.parse(xml_string)
 
-while difficulty not in ["facil", "intermedio", "dificil"]:
-    difficulty = input("Seleccione dificultad (facil/intermedio/dificil): ")
+    def select_enemy(self, enemies):
+        enemy = random.choice(enemies)
+        name = str(enemy["name"])
+        description = str(enemy["description"])
+        health = int(enemy["health"])
+        strength = int(enemy["damage"])
+        print("\n***************************************")
+        print(name)
+        print(description)
+        print("***************************************\n")
+        return enemy, health, strength
 
-if difficulty == "facil":
-    player_health = 5000
-    player_damage = random.randint(1000, 2000)
-elif difficulty == "intermedio":
-    player_health = 1000
-    player_damage = random.randint(2000, 3000)
-elif difficulty == "dificil":
-    player_health = 500
-    player_damage = random.randint(3000, 4000)
+    def load_game(self):
+        if os.path.exists(SAVE_XML):
+            with open(SAVE_XML, "r") as f:
+                game_dict = xmltodict.parse(f.read())
+                self.player = game_dict["game"]["player"]
+                self.eliminated_enemies = game_dict["game"]["eliminated_enemies"]["enemy"]
+                enemy_number = game_dict["game"]["current_enemy"]["number"]
+                enemy_health = game_dict["game"]["current_enemy"]["health"]
+                enemies = self.enemy_dict["enemies"]["enemy"]
+                self.current_enemy = enemies[int(enemy_number)]
+                self.current_enemy["health"] = int(enemy_health)
+        elif os.path.exists(SAVE_JSON):
+            with open(SAVE_JSON, "r") as f:
+                game_dict = json.loads(f.read())
+                self.player = game_dict["player"]
+                self.eliminated_enemies = game_dict["eliminated_enemies"]
+                enemy_number = game_dict["current_enemy"]["number"]
+                enemy_health = game_dict["current_enemy"]["health"]
+                enemies = self.enemy_dict["enemies"]["enemy"]
+                self.current_enemy = enemies[int(enemy_number)]
+                self.current_enemy["health"] = int(enemy_health)
+        else:
+            self.create_new_game()
 
-with open(enemy_files, "r") as f:
-    xml_string = f.read()
+    def create_new_game(self):
+        print("BIENVENIDO A NUESTRO JUEGO")
+        self.player = {
+            "name": input("Como te llamas? "),
+            "difficulty": "",
+            "health": 0,
+            "damage": 0,
+            "level": 1,
+            "xp": 0
+        }
+        while self.player["difficulty"] not in ["facil", "intermedio", "dificil"]:
+            self.player["difficulty"] = input("Seleccione dificultad (facil/intermedio/dificil): ")
 
-enemy_dict = xmltodict.parse(xml_string)
+        if self.player["difficulty"] == "facil":
+            self.player["health"] = 5000
+            self.player["damage"] = random.randint(1000, 2000)
+        elif self.player["difficulty"] == "intermedio":
+            self.player["health"] = 1000
+            self.player["damage"] = random.randint(2000, 3000)
+        elif self.player["difficulty"] == "dificil":
+            self.player["health"] = 500
+            self.player["damage"] = random.randint(3000, 4000)
 
-def select_enemy(enemies):
-    enemy = random.choice(enemies)
-    name = str(enemy["name"])
-    description = str(enemy["description"])
-    health = int(enemy["health"])
-    strength = int(enemy["damage"])
-    print("\n***************************************")
-    print(name)
-    print(description)
-    print("***************************************\n")
-    return enemy, health, strength
-
-eliminated_enemies = []
-while True:
-    remaining_enemies = [enemy for enemy in enemy_dict["enemies"]["enemy"] if enemy not in eliminated_enemies]
-    if not remaining_enemies:
-        print("¡Felicidades! ¡Has eliminado a todos los enemigos!")
-        break
-    juego, enemy_health, enemy_damage = select_enemy(remaining_enemies)	
-    print(f"\nVida del jugador: {player_health}")
-    print(f"Daño del jugador: {player_damage}")
-
-    print("\nEs el turno del jugador")
-    action = input("¿Qué quieres hacer? (ataca)")
-
-    if action == "ataca":
-        damage = random.randint(player_damage-1000, player_damage+1000)
-        enemy_health -= damage
-        print(f"Has quitado {damage} puntos de vida al enemigo.")
-    else:
-        print("Espabila papu")
-        player_health -= enemy_damage
-
-    if enemy_health <= 0:
-        print(f"{juego['name']} ha sido derrotado.")
-        eliminated_enemies.append(juego)
-    else:
-        print(f"El enemigo te ha quitado {enemy_damage} puntos de vida.")
-        player_health -= enemy_damage
-
-    if player_health <= 0:
-        print("Mala suerte papu, has perdido.")
-        break
+        self.current_enemy, enemy_health,
+def load_game_json():
+    with open('save.json') as f:
+        data = json.load(f)
+    player_data = data['player']
+    player = Player(player_data['health'], player_data['damage'], player_data['level'], player_data['xp'])
+    enemy_data = data['current_enemy']
+    current_enemy = enemies[enemy_data['number']]
+    current_enemy.health = enemy_data['health']
+    return player, current_enemy
