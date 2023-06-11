a89b989133d314c7774831749625d5510c8fab99
ADRIAVIDOSA <72547273+ADRIAVIDOSA@users.noreply.github.com>
2023-06-11 14:18:26 +0200

    Update UniverseCorp.sql
    Recuperar

--- UniverseCorp.sql
+++ UniverseCorp.sql
@@ -1,66 +1,75 @@
-CREATE TABLE Civilizations (
-	id INT PRIMARY KEY,
-	name VARCHAR(50),
-	planet VARCHAR(50),
-	dead BOOLEAN DEFAULT false
+CREATE TABLE civilizations (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    planet VARCHAR(50),
+    dead BOOLEAN DEFAULT false
 );
 
-INSERT INTO Civilizations (id, name, planet)
+INSERT INTO civilizations (id, name, planet)
 VALUES 
-	(1, 'Civilization 1', 'Planet A'),
-	(2, 'Civilization 2', 'Planet B'),
-	(3, 'Civilization 3', 'Planet B'),
-	(4, 'Civilization 4', 'Planet C'),
-	(5, 'Civilization 5', 'Planet C'),
-	(6, 'Civilization 6', 'Planet C');
+    (1, 'Civilization 1', 'Planet A'),
+    (2, 'Civilization 2', 'Planet B'),
+    (3, 'Civilization 3', 'Planet B'),
+    (4, 'Civilization 4', 'Planet C'),
+    (5, 'Civilization 5', 'Planet C'),
+    (6, 'Civilization 6', 'Planet C');
 
-CREATE VIEW PeopleCountByPlanet AS
+CREATE VIEW people_count_by_planet AS
 SELECT planet, COUNT(*) AS count
-FROM Civilizations
+FROM civilizations
 GROUP BY planet;
 
-CREATE VIEW MedicineRevenueByPlanet AS
+CREATE VIEW medicine_revenue_by_planet AS
 SELECT planet, SUM(revenue) AS medicine_revenue
-FROM Medicines
+FROM medicines
 GROUP BY planet;
 
-ALTER TABLE Civilizations
+ALTER TABLE civilizations
 ADD COLUMN dead BOOLEAN DEFAULT false;
 
 CREATE USER grimreaper;
-GRANT SELECT, UPDATE ON Civilizations TO grimreaper;
+GRANT SELECT, UPDATE ON civilizations TO grimreaper;
 
-CREATE FUNCTION GetRandomIndividualFromPlanet(planet_param VARCHAR(50)) RETURNS VARCHAR(50)
-AS $$
-DECLARE
-	individual VARCHAR(50);
+DELIMITER $$
+CREATE FUNCTION get_random_individual_from_planet(planet_param VARCHAR(50))
+RETURNS VARCHAR(50)
 BEGIN
-	SELECT name INTO individual
-	FROM Civilizations
-	WHERE planet = planet_param
-	ORDER BY random()
-	LIMIT 1;
+    DECLARE random_individual VARCHAR(50);
+    SELECT name INTO random_individual
+    FROM civilizations
+    WHERE planet = planet_param
+    ORDER BY RAND()
+    LIMIT 1;
+    RETURN random_individual;
+END$$
+DELIMITER ;
 
-	RETURN individual;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE PROCEDURE KillUser(user_param VARCHAR(50))
-LANGUAGE plpgsql
-AS $$
+DELIMITER $$
+CREATE PROCEDURE kill_user(user_param VARCHAR(50))
 BEGIN
-	IF EXISTS (SELECT 1 FROM Civilizations WHERE name = user_param AND dead = true) THEN
-		RAISE NOTICE 'You cannot kill % again.', user_param;
-	ELSE
-		UPDATE Civilizations
-		SET dead = true
-		WHERE name = user_param;
+    DECLARE user_name VARCHAR(50);
+    SET user_name = (SELECT name FROM civilizations WHERE name = user_param);
 
-		RAISE NOTICE 'HA MUERTO... %', user_param;
-	END IF;
-END;
-$$;
+    IF EXISTS (SELECT 1 FROM civilizations WHERE name = user_param AND dead = true) THEN
+        SELECT CONCAT(
+            'No puedes matar otra vez a "',
+            user_name,
+            '".'
+        ) AS Resultado;
+    ELSE
+        UPDATE civilizations
+        SET dead = true
+        WHERE name = user_param;
+        SELECT CONCAT(
+            'HA MUERTO... "',
+            user_name,
+            '".'
+        ) AS Resultado;
+    END IF;
+END$$
+DELIMITER ;
 
-SET ROLE grimreaper;
-SELECT GetRandomIndividualFromPlanet('Planet A');
-CALL KillUser('Civilization 1');
+SET @user_role = 'grimreaper';
+SET @planet = 'Planet A';
+SELECT get_random_individual_from_planet(@planet);
+CALL kill_user('Civilization 1');
